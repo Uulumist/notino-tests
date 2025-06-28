ca250801a48b2ef6ce56be9efb9918e9fbe89db3
Uulumist <uulumist@gmail.com>
2025-06-28 19:56:41 +0300

    Added test for checking top bar elements link

--- tests/notino.spec.ts
+++ tests/notino.spec.ts
@@ -1,19 +1,18 @@
 import { test, expect } from "@playwright/test";
 
-const elements = [
-  {
-    locator: (page) =>
-      page.getByRole("link", { name: "Tootenäidiseid täis ilukarp" }),
-  },
-  {
-    locator: (page) => page.getByRole("link", { name: "Meiega leiad oma" }),
-  },
+const top_bar_elements = [
   {
     locator: (page) => page.getByTestId("call-info-wrapper"),
+    name: "phone number",
   },
   {
     locator: (page) =>
       page.getByRole("link", { name: "Kohaletoimetamise teave" }),
+    name: "delivery info",
+    attribute: {
+      type: "href",
+      value: "/saadetise-info/",
+    },
   },
 ];
 
@@ -22,12 +21,23 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Main page elements", () => {
-
-  test("top bar elements", async ({ page }) => {
-    for (const { locator } of elements) {
-      await test.step("top bar", async () => {
+  test("top bar elements visibility", async ({ page }) => {
+    for (const { locator, name } of top_bar_elements) {
+      await test.step(`Check visibility of: ${name}`, async () => {
         await expect.soft(locator(page)).toBeVisible();
       });
+    }
+  });
+
+  test("top bar elements link", async ({ page }) => {
+    for (const { locator, name, attribute } of top_bar_elements) {
+      if (name === "delivery info") {
+        await test.step(`Check href attribute of: ${name}`, async () => {
+          await expect
+            .soft(locator(page))
+            .toHaveAttribute(attribute.type, attribute.value);
+        });
+      }
     }
   });
 });
